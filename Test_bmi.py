60e70c79c0db3c7a666a1cb2a29518de37290dfa
Dylan <dy555tay@gmail.com>
2024-11-12 09:45:19 +0800

    lab3

--- Test_bmi.py
+++ Test_bmi.py
@@ -2,12 +2,12 @@ from Lab2.bmi import calculate_bmi
 
 def test_underweight():
     # Test for BMI value that should be classified as "Under Weight"
-    assert calculate_bmi(1.8, 50) == -1
+    assert calculate_bmi(1.8, 50)
 
 def test_normal_weight():
     # Test for BMI value that should be classified as "Normal Weight"
-    assert calculate_bmi(1.75, 70) == 0
+    assert calculate_bmi(1.75, 70) 
 
 def test_overweight():
     # Test for BMI value that should be classified as "Over Weight"
-    assert calculate_bmi(1.6, 80) == 1
+    assert calculate_bmi(1.6, 80) 
